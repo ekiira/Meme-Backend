32cfd3747eabdc8d8086b7d81fb00b1f56e2c724
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-04-06 21:03:14 +0100

    db connected schema setup

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+const Meme = require('./models/meme')
+
+const databaseUrl = 'mongodb://localhost:27017/meme-generator';
+mongoose.connect(databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.get('/', (req, res) => {
     res.send('HomePage Holla!!')
 });
+
+app.get('/view', (req, res) => {
+    Meme.find({}, (err, allMemes) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(allMemes)
+        res.send(allMemes);
+      }
+    });
+  });
+  
 
 app.listen(3001, () => {
     console.log('server has started')
