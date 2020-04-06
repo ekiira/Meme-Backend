aa3fb8ee72df1f9b6c16d9a5e2e1e339a856c078
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-04-06 21:57:07 +0100

    received data from frontend

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const Meme = require('./models/meme')
 
 const databaseUrl = 'mongodb://localhost:27017/meme-generator';
@@ -9,6 +11,9 @@ mongoose.connect(databaseUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('HomePage Holla!!')
@@ -26,6 +31,24 @@ app.get('/view', (req, res) => {
   });
   
 
+  app.post('/add', (req, res) => {
+    const caption = req.body.caption;
+    const url = req.body.imgUrl;
+    const upload = req.body.imgUpload;
+    const newMemes = {
+      caption,
+      imageUrl: url,
+      imageUpload: upload,
+    };
+    Meme.create(newMemes, (err, addMemes) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect('/view');
+        console.log('this is the create page', addMemes);
+      }
+    });
+  })
 app.listen(3001, () => {
     console.log('server has started')
 });
